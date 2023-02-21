878d2a521353ac1ef4889ee2cff3a70088244777
Brag <brangtsawmaung89@gmail.com>
2023-02-21 16:18:19 +0630

    page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,8 +16,7 @@ function About() {
     
       const { View } = useLottie(options);
   return (
-    <Animation>
-    <div className='flex-col xsm:w-[100%] xsm:h-screen xsm:py-10 md:py-3 xsm:px-4 lg:px-10 xsm:items-center md:items-start xsm:justify-center overflow-y-scroll'>
+    <div className='flex-col xsm:w-[100%] xsm:h-[95%] xsm:py-10 md:py-3  xsm:px-4 lg:px-10 xsm:items-center md:items-start xsm:justify-center overflow-y-scroll'>
         <div className='flex justify-center items-center w-full xsm:mb-4'>
             <div className='xsm:w-[90%] sm:w-[45%] ' >
                 <Animation>
@@ -46,19 +45,7 @@ function About() {
             <p className='xsm:text-sm md:text-md text-justify'>This quiz app is an interactive and engaging platform that allows users to test their knowledge on English grammar. The app is designed with a user-friendly interface and offers a variety of question formats such as multiple choice, true or false (coming soon), and fill in the blank (coming soon).</p>
         </div>
         </ AnimateHeight >
-        {/* {data.map(item => {
-            return(
-            <Animation>
-            <div className={`bg-white xsm:p-4 mb-3 ${open ? "h-auto" : "h-[60px] overflow-hidden"} rounded-2xl transition-all relative duration-300`}>
-                <div className='absolute top-5 right-4' onClick={() => setOpen(!open)}>{open ? <FaChevronDown /> : <FaChevronUp />}</div>
-                <h2 className='xsm:text-md lg:text-lg font-bold text-[#3d4d57] mb-4'>{item.title}</h2>
-                <p className='xsm:text-sm md:text-md text-justify'>{item.paragraph}</p>
-            </div>
-            </Animation>
-            )
-        })} */}
     </div>
-    </Animation>
   )
 }
 
